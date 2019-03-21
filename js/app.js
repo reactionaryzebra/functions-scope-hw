203765098d577150e1066bebdcde05e236bdee24
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-21 14:59:46 -0700

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//Section 1
+//1) A parameter is a variable accepted and used by a function.  Parameters are named when a function is declared
+//An argument is a value that you pass to a function when you call it.
+//2) Console.log simply logs values to the developer console.  Return will serve as the output of a function to be
+//used in other parts of your code later.
+//3) A function returning a value means that function can be used later on in your code to affect other logic.
